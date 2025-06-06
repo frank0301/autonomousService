743cfc6221481f573125edaf406cdcb8c610dc97
yipeng <2698678406@qq.com>
2025-06-06 11:45:47 +0300

    update coordinate once fixed.

--- src/detect_vl/detect_vl/start_service.py
+++ src/detect_vl/detect_vl/start_service.py
@@ -10,13 +10,11 @@ this node func:
 3 Then publish the coordinates_diff
 ############################
 '''
-
-
 import rclpy
 from rclpy.node import Node
 
 from sensor_msgs.msg import Image,CompressedImage
-from std_msgs.msg import Int32MultiArray,Float32MultiArray
+from std_msgs.msg import Int32MultiArray,Float32MultiArray,String
 
 from cv_bridge import CvBridge
 import cv2
@@ -30,6 +28,7 @@ from scripts.service_vl import GroundingDINOInfer
 import scripts.service_lm as lm
 import scripts.ans2json as ans2json
 import os
+
 os.environ["QT_QPA_PLATFORM"] = "xcb"
 from common_interface.msg import RectDepth
 class ServiceNode(Node):
@@ -39,15 +38,14 @@ class ServiceNode(Node):
 
         self.rgb_image = None
         self.depth_image = None
-
+        self.robot_state = None
         self.rect = None
         
-
         self.obj_list=None
         self.act_list=None
 
         self.VL = GroundingDINOInfer()
-        time.sleep(10)
+        
         self.create_subscription(CompressedImage, '/camera/camera/color/image_raw/compressed', self.rgb_callback, 30)
         # self.create_subscription(CompressedImage, '/camera/camera/depth/image_rect_raw/compressed', self.depth_callback, 10)
         self.create_subscription(Image, '/camera/camera/depth/image_rect_raw', self.depth_callback, 30)
@@ -55,11 +53,15 @@ class ServiceNode(Node):
         self.target_pub = self.create_publisher(RectDepth, 'task/rect_depth', 10)
         self.get_logger().info("ServiceNode node started, waiting for image...")
 
+        self.create_subscription(String, '/robot_state',self.robot_state_update_callback, 10)
+        self.showImgTimer=self.create_timer(0.3,self.showImg)
+    def showImg(self):
+        if self.rgb_image is not None:
+            cv2.imshow("rgb img", self.rgb_image)
+            cv2.waitKey(1)
     def rgb_callback(self, msg):
         try:
             self.rgb_image = self.bridge.compressed_imgmsg_to_cv2(msg, desired_encoding='bgr8')
-            # cv2.imshow("rs_img", self.rgb_image)
-            # cv2.waitKey(100)
         except Exception as e:
             self.get_logger().error(f"图像处理失败: {e}")
 
@@ -78,6 +80,10 @@ class ServiceNode(Node):
         except Exception as e:
             self.get_logger().error(f"Depth image failed to transfer: {e}")
 
+    def robot_state_update_callback(self, msg):
+        self.robot_state = msg.data
+        self.get_logger().info(f"{msg.data}")
+
     def pix2world(self, pix_xy):
             if self.depth_image is None:
                 self.get_logger().warn("⚠️ Depth image not yet received.")
@@ -86,13 +92,13 @@ class ServiceNode(Node):
             center_depth_mm = self.depth_image[pix_xy[1], pix_xy[0]]  # Access depth image using (row, col)
             center_depth_m = center_depth_mm / 1000.0
 
-            self.get_logger().info(f"📏 Center depth at ({pix_xy[0]},{pix_xy[1]}): {center_depth_m:.3f} m")
+            # self.get_logger().info(f"📏 Center depth at ({pix_xy[0]},{pix_xy[1]}): {center_depth_m:.3f} m")
             cx,cy = 319.47, 247
             fx,fy = 615.53, 615.53
             pix_x, pix_y = pix_xy 
             wY = (pix_x - cx) * center_depth_m / fx
             wX = center_depth_m #(pix_y - cy) * center_depth_m / fy
-            self.get_logger().info(f"wY:{wY},wX:{wX}")
+            # self.get_logger().info(f"wY:{wY},wX:{wX}")
             return center_depth_m,wX,-wY
 
     
@@ -105,17 +111,6 @@ def main(args=None):
     try:
         while True:
             msg = RectDepth()          
-            # msg.frame = time.time()
-
-            # msg.depth = 3.0 #dis
-            # msg.coordinate_diff = Float32MultiArray()
-
-
-            # msg.coordinate_diff.data = [3.0, 0.7]#[wx, wy]
-            
-            # node.target_pub.publish(msg)
-            # time.sleep(3)
-            # continue
             if node.rgb_image is None:
                 time.sleep(1)
                 continue
@@ -152,10 +147,17 @@ def main(args=None):
                     dis,wx,wy = node.pix2world(center)
                     if dis is None:
                         continue
-                    print(f"dis={dis},coordinate=({wx},{wy})")
+                    # print(f"dis={dis},coordinate=({wx},{wy})")
+                    
                     if dis == 0:
                         continue
-                    if dis < 0.5:
+                    if dis < 3 and node.robot_state == "navigating":                      
+                        print("waitting reachGoal")
+                        while node.robot_state != "reachGoal":
+                            print(node.robot_state)
+                            # rclpy.spin_once(node)
+                            time.sleep(0.5)
+                        print("reached goal!")
                         idx += 1
                     else:
                         msg.rect = Int32MultiArray()
@@ -166,26 +168,32 @@ def main(args=None):
                         
                         msg.frame = time.time()
 
-                        msg.depth = 3.0 #dis
+                        msg.depth = dis
                         msg.coordinate_diff = Float32MultiArray()
-
-
-                        msg.coordinate_diff.data = [3.0, 0.7]#[wx, wy]
+                        msg.coordinate_diff.data = [wx, wy]
                         
                         node.target_pub.publish(msg)
+                        print("update target!")
                         time.sleep(3)
                 elif act and act.lower() != "null":
                     msg.coodinate_diff = [0, 0]
                     msg.theta = float(act)
-                    node.target_pub.publish(msg)
+                    while node.robot_state != "turning":
+                        node.target_pub.publish(msg)
+                        # rclpy.spin_once(node)
+                        time.sleep(0.5)
                     print("waiting turning")
-                    idx += 1
                     time.sleep(5)
-                
+                    while node.robot_state != "reachGoal":
+                        rclpy.spin_once(node)
+                        time.sleep(0.5)
+                    idx += 1             
                 else:
                     node.get_logger().warn("both null, jump over it!")
                     idx+=1
+            print(idx)
             node.get_logger().info("success!")
+            # rclpy.spin_once(node)
     except KeyboardInterrupt:
         print("⛔ 退出程序")
     finally:
